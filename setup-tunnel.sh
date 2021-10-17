4cf48409efe053a76ffa2488bdb6745dc37213cf
Shaunak G <gadkari.shaunak@gmail.com>
2021-10-17 19:06:28 +1100

    Create setup-tunnel.sh

---
+++ setup-tunnel.sh
@@ -0,0 +1,26 @@
+
+echo "=== Setting up tunnelling and port forward tools ==="
+echo "[INFO] Installing Cloudflare Tunnel"
+
+curl -L -o "cftunnel.deb" "https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb"
+dpkg -i cftunnel.dev
+rm -f cftunnel.deb
+
+echo "[INFO] Cloudflare Tunnel installed."
+echo "[ACTN] Please login with your Cloudflare account!"
+
+cloudflared tunnel login
+
+echo "[INFO] Login complete."
+
+function mktun {
+  
+  echo "=== Tunnel will forward port $1 to http://devtunnel--$2.srg.id.au ==="
+  cloudflared tunnel --hostname "devtunnel--$2.srg.id.au" --name "devtunnel-$1-$2" --url "localhost:$1"
+  
+}
+
+echo "[INFO] Tunnel is good to go!"
+echo "Use tunnel functionality like this:"
+echo "    mktun 8080 mytunnel"
+echo "to forward localhost:8080 to http://devtunnel-mytunnel.srg.id.au"
