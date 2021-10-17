5115360dc1c19c84867ce988b231799a6e9cc451
Shaunak G <gadkari.shaunak@gmail.com>
2021-10-17 20:04:07 +1100

    Create setup-msft-marketplace.sh

---
+++ setup-msft-marketplace.sh
@@ -0,0 +1,8 @@
+export SERVICE_URL="https://marketplace.visualstudio.com/_apis/public/gallery"
+export ITEM_URL="https://marketplace.visualstudio.com/items"
+
+function getext {
+  code-server --install-extension $1
+}
+
+echo "Using Microsoft endpoints for extension install. Run getext to install extension, or use GUI"
