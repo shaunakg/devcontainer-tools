fde9265e10a32b86b7e3a29ee76317ca52b2e60e
Shaunak G <gadkari.shaunak@gmail.com>
2021-10-18 13:46:37 +1100

    Create devtools.sh

---
+++ devtools.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+echo "[INFO] Setting timezone"
+sudo ln -sf /usr/share/zoneinfo/Australia/Melbourne /etc/localtime
+
+echo "[INFO] Updating apt-get"
+sudo apt-get update
+
+echo "[INFO] Installing core packages (e.g gnupg, curl)"
+sudo apt-get install curl gnupg -y
+
+echo "[INFO] Adding repo key for yarn into package repository"
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update -y
+
+echo "[INFO] Installing tzdata"
+sudo apt-get install tzdata -y
+
+echo "[INFO] Installing python3.10"
+sudo apt-get install python3.10 -y
+sudo apt-get install aptitude -y
+
+echo "[INFO] Adding node.js package repo to system"
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+
+echo "[INFO] Installing node.js and yarn"
+sudo aptitude install nodejs -y
+sudo apt-get update
+sudo apt-get install -y yarn
+
+echo "[INFO] Done"
