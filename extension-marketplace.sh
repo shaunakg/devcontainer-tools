eb89b69b9ec3be0aab40409c5dd03b8c4586eb5f
Shaunak G <gadkari.shaunak@gmail.com>
2021-10-19 11:53:20 +1100

    Update and rename setup-msft-marketplace.sh to extension-marketplace.sh

--- extension-marketplace.sh
+++ extension-marketplace.sh
@@ -1,9 +1,7 @@
 export SERVICE_URL="https://marketplace.visualstudio.com/_apis/public/gallery"
 export ITEM_URL="https://marketplace.visualstudio.com/items"
 
-function getext {
-  code-server --install-extension $1
-}
+chmod +x /home/coder/tools/getext
+ln -s /home/coder/tools/getext /usr/bin/getext
 
 echo "Using Microsoft endpoints for extension install. Run getext to install extension, or use GUI"
-export -f getext
