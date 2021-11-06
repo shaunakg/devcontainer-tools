d34702bfdabc0093c46a2aaa3eff3dc9d22dd479
Shaunak G <gadkari.shaunak@gmail.com>
2021-11-06 16:29:09 +1100

    Update tailscale.sh

--- tailscale.sh
+++ tailscale.sh
@@ -3,4 +3,5 @@
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/bionic.gpg | sudo apt-key add -
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/bionic.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 sudo apt-get install tailscale
-sudo tailscale up
+
+sudo tailscale up --authkey "$1"
