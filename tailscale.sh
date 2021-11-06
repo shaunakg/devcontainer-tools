64d4b0844c402efd982f3a6bf7ec9775d3021d78
Shaunak G <gadkari.shaunak@gmail.com>
2021-11-06 16:45:36 +1100

    Update tailscale.sh

--- tailscale.sh
+++ tailscale.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/bionic.gpg | sudo apt-key add -
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/bionic.list | sudo tee /etc/apt/sources.list.d/tailscale.list
-sudo apt-get install tailscale
+go() {
+  curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.gpg | sudo apt-key add -
+  curl -fsSL https://pkgs.tailscale.com/stable/debian/bullseye.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 
-sudo tailscale up --authkey "$1"
+  sudo apt-get update
+  sudo apt-get install tailscale
+
+  sudo tailscale up --authkey "$1"
+}
+
+go 2>&1 >> /home/coder/tools/tailscale.log
