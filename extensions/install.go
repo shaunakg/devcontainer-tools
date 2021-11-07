b7a30f4d8845fd91eb0d9c4c35ac1562a483cb46
Shaunak G <gadkari.shaunak@gmail.com>
2021-11-07 13:10:24 +1100

    Create install.go

---
+++ extensions/install.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os/exec"
+)
+
+type Extensions []string
+
+func main() {
+
+	var data Extensions
+	file, err := ioutil.ReadFile("/home/coder/tools/extensions/extensions.json")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(file, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, element := range data {
+
+		app := "code-server"
+		installFlag := "--install-extension"
+
+		fmt.Printf("Installing %s\n", element)
+
+		cmd := exec.Command(app, installFlag, element)
+		stdout, err := cmd.Output()
+
+		if err != nil {
+			fmt.Println(err.Error())
+		} else {
+			// Print the output
+			fmt.Println(string(stdout))
+		}
+
+	}
+
+}
